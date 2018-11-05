d0c09e9d41f0a13a71551c51fc5672a2562331b7
JacobMartinezN <m.a.r.t.i.n.e.z.2.2470@gmail.com>
2018-11-05 00:51:05 -0500

    add option done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {todos} from "./todos.json"
+import TodoForm from './components/TodoForm';
 
 class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      todos
+    }
+    this.handleAddTodo = this.handleAddTodo.bind(this)
+  }
+
+  handleAddTodo(todo){
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
   render() {
+
+    const todos = this.state.todos.map((todo , i) => {
+      return(
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2"> {todo.priority}</span>
+            </div>
+            <div className="card-body">
+              <p>{todo.description}</p>
+              <b>{todo.responsible}</b>
+            </div>
+          </div>
+        </div>
+      )
+    })
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav className="navbar navbar-dark bg-dark">
+                <a className="text-white">
+                    Task
+                </a>
+                <span className="badge badge-pill badge-light">
+                  { this.state.todos.length }
+                </span>
+            </nav>
+        <img src={ logo } className="App-logo" alt="logo" />
+        <TodoForm onAddTodo={this.handleAddTodo} />
+        <div className="container">
+          <div className="row mt-4">
+          {todos}
+          </div>
+        </div>
+        
       </div>
+
+     
     );
   }
 }
